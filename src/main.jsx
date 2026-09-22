40b582a24acdcecaf3624962aecc258e63adbd71
ahmed8013 <divelangel2@gmail.com>
2026-09-22 08:57:46 +0000

    Integrate the live world clock into the studio as a responsive floating command-center widget.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import WorldClock from './components/WorldClock';
 import './styles.css';
+
+function StudioWithClock() {
+  return (
+    <>
+      <App />
+      <div className="world-clock-dock">
+        <WorldClock />
+      </div>
+    </>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <StudioWithClock />
   </StrictMode>
 );
